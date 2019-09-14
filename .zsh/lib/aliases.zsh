71aa3a4c941e686c6e0a479d3346cb2254ab1d2e
Sebastian Lauber <dev@slauber.de>
2019-09-14 10:57:57 +0200

    Changed crontab to crontab -i, protecting your crontab since 2019

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -1,10 +1,8 @@
 alias ll='ls -la'
 alias lh='ls -lah'
 alias ...='cd ../..'
-alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
-alias vinfo='mplayer -vo null -ao null -identify -frames 0'
-alias gut='git'
-alias fire='git commit -am "BURNING DOWN" && git push -f origin master'
+alias ....='cd ../../..'
+alias crontab='crontab -i'
 
 function mcd() {
     mkdir $1 && cd $1
